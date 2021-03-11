45c488e7fecbd49c9afc0d8a1b05d444dfa6f9b5
AndresVidalesRodriguez <47193023+AndresVidalesRodriguez@users.noreply.github.com>
2021-03-11 17:00:08 -0600

    Add files via upload

---
+++ b.js
@@ -0,0 +1,4 @@
+console.debug("Este es un mensaje del debug");
+console.warn("Este es un mensaje de aviso");
+console.error("Este es un mensaje de error");
+console.info("Este es un mensaje de informacion");
